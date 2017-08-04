e8b2c638b8a88517746f002227304928fc9ca37b
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-08-04 21:20:09 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+
+// /**
+//  * Created by vagrant on 8/4/17.
+//  */
+//Onclick angry_maki, console log
+
+var makiObject= document.getElementById("sushi-box-1");
+makiObject.onhover = function () {
+    console.log("HEllo!");
+}
