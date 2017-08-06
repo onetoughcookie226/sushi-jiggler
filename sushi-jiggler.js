8f57288221aed836a383f2b802176c137ca8e21a
onetoughcookie226 <onetoughcookie226@gmail.com>
2017-08-06 12:39:22 +0200

    Inital commit

---
+++ sushi-jiggler.js
@@ -0,0 +1,36 @@
+
+/*var sushi= ['chopsticks','angry-maki','','']*/
+
+
+//Enlarge the selected img
+//get the target element and give it a new class
+/*function grow (sushi) {
+    console.log ( document.getElementById( sushi ));
+    }*/
+
+//change the size of the the selected img
+//get the target element and give it a new class
+//if the target element class is grow change it back to the original
+//else give it a grow class
+
+// $("#sushi-box-1").addClass("animated shake");
+
+// function resize (sushi) {
+//     //target the dom object element with id sushi
+// var currentImgObject = document.getElementById (sushi);
+//     //check if the current className property of the currentImgObject is 'grow'
+//    if (currentImgObject.className === 'grow') {
+//     //change the value of the property className of the currentImgObject to 'shrink'
+//     currentImgObject.className = 'shrink';
+//    } else {
+//    //change the value of the property className of the currentImgObject to 'grow'
+//     currentImgObject.className = 'grow';
+//    }
+
+
+
+
+
+//    document.getElementById( sushi ).className = ' grow';
+//    console.log (sushi);
+}
